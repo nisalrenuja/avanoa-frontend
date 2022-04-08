495137614030e841d28e1f8fe3299a63f99be594
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2022-04-08 22:37:58 +0530

    Frontend changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import Dashboard from './pages/Dashboard'
-import Home from './pages/Home'
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-	return (
-		<div>
-			<BrowserRouter>
-				<Route path="/" exact component={Home} />
-				<Route path="/login" exact component={Login} />
-				<Route path="/register" exact component={Register} />
-				<Route path="/dashboard" exact component={Dashboard} />
-			</BrowserRouter>
-		</div>
-	)
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
