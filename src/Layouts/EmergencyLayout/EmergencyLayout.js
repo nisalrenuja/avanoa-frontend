0c5198a8ada98e62116a02e40d3e24c5182735ed
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2022-05-13 04:46:08 +0530

    Emergency file added

--- src/Layouts/EmergencyLayout/EmergencyLayout.js
+++ src/Layouts/EmergencyLayout/EmergencyLayout.js
@@ -1,36 +1,108 @@
-import Sidebar from "../../components/Sidebar/Sidebar";
-import Appbar from "../../components/Appbar/Appbar";
-import { useState } from "react";
+import React from "react";
+import { Row, Col, Form, Input, Button, Space, notification } from "antd";
+import "antd/dist/antd.css";
 
-const EmergencyLayout = () => {
-  const [sidebar, setSidebar] = useState(false);
 
-  const handleSidebar = () => {
-    setSidebar(!sidebar);
+const { TextArea } = Input;
+require('dotenv').config();
+const sgMail = require("@sendgrid/mail");
+const apiKey = `${process.env.SENDGRID_API_KEY}`;
+console.log("SendGrid key ", apiKey);
+
+function EmergencyLayout() {
+  const [form] = Form.useForm();
+
+  const onComplete = (fields) => {
+    const message = {
+      to: "nrenuja@gmail.com",
+      from: fields.email,
+      subject: fields.subject,
+      html: `
+      <p><strong>Name:</strong> ${fields.name}</p>
+      <p>${fields.message}</p>`,
+    };
+
+    sgMail
+      .send(message)
+      .then(() => {
+        form.resetFields();
+        console.log("Email Sent!");
+        notification.open({
+          message: "Message successfu!",
+          description: "We have successfully received your email.",
+        });
+      })
+      .catch((error) => {
+        console.error("Error: ", error);
+      });
   };
 
   return (
-    <div className="profilelayout">
-      {/* appbar */}
-      <Appbar handleSidebar={handleSidebar} />
-      {/* sidebar */}
-      <div
-        className={
-          sidebar ? "profilelayout_sidebar open" : "profilelayout_sidebar"
-        }
-      >
-        <Sidebar />
-      </div>
-
-      {/* content */}
-      <div className="profilelayout_content">
-        {/* <Speech /> */}
-        {/* feed */}
-        <div className="profilelayout_content-feed"></div>
-        {/* profile */}
-      </div>
-    </div>
+    <Row gutter={24} style={{ padding: "30px" }}>
+      <Col xl={12}>
+        <Form layout="vertical" form={form} onFinish={onComplete}>
+          <Form.Item
+            name="name"
+            label="Name"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="email"
+            label="Email"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="subject"
+            label="Subject"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name="message"
+            label="Message"
+            rules={[
+              {
+                required: true,
+              },
+            ]}
+          >
+            <TextArea />
+          </Form.Item>
+          <Form.Item>
+            <Space>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+              <Button
+                type="secondary"
+                htmlType="submit"
+                onClick={(e) => form.resetFields()}
+              >
+                Clear
+              </Button>
+            </Space>
+          </Form.Item>
+        </Form>
+      </Col>
+    </Row>
   );
-};
+}
 
 export default EmergencyLayout;
