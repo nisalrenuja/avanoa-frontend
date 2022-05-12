053446946feef302d157c9047e84f7a58f78b988
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2022-05-13 00:50:26 +0530

    Emergency template added

---
+++ src/Layouts/EmergencyLayout/EmergencyLayout.js
@@ -0,0 +1,36 @@
+import Sidebar from "../../components/Sidebar/Sidebar";
+import Appbar from "../../components/Appbar/Appbar";
+import { useState } from "react";
+
+const EmergencyLayout = () => {
+  const [sidebar, setSidebar] = useState(false);
+
+  const handleSidebar = () => {
+    setSidebar(!sidebar);
+  };
+
+  return (
+    <div className="profilelayout">
+      {/* appbar */}
+      <Appbar handleSidebar={handleSidebar} />
+      {/* sidebar */}
+      <div
+        className={
+          sidebar ? "profilelayout_sidebar open" : "profilelayout_sidebar"
+        }
+      >
+        <Sidebar />
+      </div>
+
+      {/* content */}
+      <div className="profilelayout_content">
+        {/* <Speech /> */}
+        {/* feed */}
+        <div className="profilelayout_content-feed"></div>
+        {/* profile */}
+      </div>
+    </div>
+  );
+};
+
+export default EmergencyLayout;
