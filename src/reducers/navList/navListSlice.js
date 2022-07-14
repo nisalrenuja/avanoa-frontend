5604ca993ebb828cbbb685ef76c09b5e4e6c0a40
nipunbandara <nipun.bandara@outlook.com>
2022-07-14 11:40:02 +0530

    navigation list updated for new changes

--- src/reducers/navList/navListSlice.js
+++ src/reducers/navList/navListSlice.js
@@ -3,8 +3,8 @@ import { createSlice } from '@reduxjs/toolkit'
 export const navListSlice = createSlice({
   name: 'navList',
   initialState: {
-    tree: [2,4,3,3,4,2,5,9,9,9],
-    treeLayers : [1,3,0,7,0,0,0,10,19,23],
+    tree: [2, 5,5, 8,4,3,5,5, 10,10,9,9,9,9,9,8, 1,1,1,2,3],
+    treeLayers : [1, 3,8, 13,0,0,0,0, 21,31,41,50,60,70,80,89, 96,97,98,99,101],
     index : 0,
     layer : 0
 },
