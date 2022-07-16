8a3626f2072d32129fbc76b5ff3d2a4f639ae0f8
nuduja <nudujabandara@gmail.com>
2022-07-16 21:05:16 +0530

    PB Sample Audio Files Added

--- src/components/PhraseBubble/PhraseBubble.js
+++ src/components/PhraseBubble/PhraseBubble.js
@@ -6,6 +6,12 @@ import { updateIndex } from "../../reducers/navList/navListSlice";
 import { setCount } from "../../reducers/counter/counterSlice";
 
 const audioClips = [
+  // Siren - need to add
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651477331/2022-05-02_13-11-14_wqvdvs.mp3",
+    label: "ආයුබෝවන්",
+  },
+  // Slide 1 Audio
   {
     sound: "https://res.cloudinary.com/avanoa/video/upload/v1651477331/2022-05-02_13-11-14_wqvdvs.mp3",
     label: "ආයුබෝවන්",
@@ -42,7 +48,273 @@ const audioClips = [
     sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482600/2022-05-02_14-39-21_vxrmj1.mp3",
     label: "කොහොමද?",
   },
+
+  // Slide 2 Audio
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651477331/2022-05-02_13-11-14_wqvdvs.mp3",
+    label: "ආයුබෝවන්",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651477331/2022-05-02_13-11-14_wqvdvs.mp3",
+    label: "ඔව්",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651481915/2022-05-02_14-27-37_bugyqg.mp3",
+    label: "නැහැ",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482089/2022-05-02_14-30-09_y5ilia.mp3",
+    label: "සුබ උදෑසනක්",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482230/2022-05-02_14-32-00_zdbvbg.mp3",
+    label: "සුබ රාත්‍රියක්",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482322/2022-05-02_14-34-35_q2x4aw.mp3",
+    label: "නිදිමතයි",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482461/2022-05-02_14-36-44_r5dtsf.mp3",
+    label: "තිබහයි",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482528/2022-05-02_14-38-12_o4oslm.mp3",
+    label: "බඩිගිනි",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482600/2022-05-02_14-39-21_vxrmj1.mp3",
+    label: "කොහොමද?",
+  },
+
+  // Slide 3 Audio
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651477331/2022-05-02_13-11-14_wqvdvs.mp3",
+    label: "ආයුබෝවන්",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651477331/2022-05-02_13-11-14_wqvdvs.mp3",
+    label: "ඔව්",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651481915/2022-05-02_14-27-37_bugyqg.mp3",
+    label: "නැහැ",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482089/2022-05-02_14-30-09_y5ilia.mp3",
+    label: "සුබ උදෑසනක්",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482230/2022-05-02_14-32-00_zdbvbg.mp3",
+    label: "සුබ රාත්‍රියක්",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482322/2022-05-02_14-34-35_q2x4aw.mp3",
+    label: "නිදිමතයි",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482461/2022-05-02_14-36-44_r5dtsf.mp3",
+    label: "තිබහයි",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482528/2022-05-02_14-38-12_o4oslm.mp3",
+    label: "බඩිගිනි",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482600/2022-05-02_14-39-21_vxrmj1.mp3",
+    label: "කොහොමද?",
+  },
+
+  // Slide 4 Audio
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651477331/2022-05-02_13-11-14_wqvdvs.mp3",
+    label: "ආයුබෝවන්",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651477331/2022-05-02_13-11-14_wqvdvs.mp3",
+    label: "ඔව්",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651481915/2022-05-02_14-27-37_bugyqg.mp3",
+    label: "නැහැ",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482089/2022-05-02_14-30-09_y5ilia.mp3",
+    label: "සුබ උදෑසනක්",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482230/2022-05-02_14-32-00_zdbvbg.mp3",
+    label: "සුබ රාත්‍රියක්",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482322/2022-05-02_14-34-35_q2x4aw.mp3",
+    label: "නිදිමතයි",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482461/2022-05-02_14-36-44_r5dtsf.mp3",
+    label: "තිබහයි",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482528/2022-05-02_14-38-12_o4oslm.mp3",
+    label: "බඩිගිනි",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482600/2022-05-02_14-39-21_vxrmj1.mp3",
+    label: "කොහොමද?",
+  },
+
+  // Slide 5 Audio
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651477331/2022-05-02_13-11-14_wqvdvs.mp3",
+    label: "ආයුබෝවන්",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651477331/2022-05-02_13-11-14_wqvdvs.mp3",
+    label: "ඔව්",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651481915/2022-05-02_14-27-37_bugyqg.mp3",
+    label: "නැහැ",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482089/2022-05-02_14-30-09_y5ilia.mp3",
+    label: "සුබ උදෑසනක්",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482230/2022-05-02_14-32-00_zdbvbg.mp3",
+    label: "සුබ රාත්‍රියක්",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482322/2022-05-02_14-34-35_q2x4aw.mp3",
+    label: "නිදිමතයි",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482461/2022-05-02_14-36-44_r5dtsf.mp3",
+    label: "තිබහයි",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482528/2022-05-02_14-38-12_o4oslm.mp3",
+    label: "බඩිගිනි",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482600/2022-05-02_14-39-21_vxrmj1.mp3",
+    label: "කොහොමද?",
+  },
+
+  // Slide 6 Audio
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651477331/2022-05-02_13-11-14_wqvdvs.mp3",
+    label: "ආයුබෝවන්",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651477331/2022-05-02_13-11-14_wqvdvs.mp3",
+    label: "ඔව්",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651481915/2022-05-02_14-27-37_bugyqg.mp3",
+    label: "නැහැ",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482089/2022-05-02_14-30-09_y5ilia.mp3",
+    label: "සුබ උදෑසනක්",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482230/2022-05-02_14-32-00_zdbvbg.mp3",
+    label: "සුබ රාත්‍රියක්",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482322/2022-05-02_14-34-35_q2x4aw.mp3",
+    label: "නිදිමතයි",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482461/2022-05-02_14-36-44_r5dtsf.mp3",
+    label: "තිබහයි",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482528/2022-05-02_14-38-12_o4oslm.mp3",
+    label: "බඩිගිනි",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482600/2022-05-02_14-39-21_vxrmj1.mp3",
+    label: "කොහොමද?",
+  },
+  // Slide 7 Audio
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651477331/2022-05-02_13-11-14_wqvdvs.mp3",
+    label: "ආයුබෝවන්",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651477331/2022-05-02_13-11-14_wqvdvs.mp3",
+    label: "ඔව්",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651481915/2022-05-02_14-27-37_bugyqg.mp3",
+    label: "නැහැ",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482089/2022-05-02_14-30-09_y5ilia.mp3",
+    label: "සුබ උදෑසනක්",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482230/2022-05-02_14-32-00_zdbvbg.mp3",
+    label: "සුබ රාත්‍රියක්",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482322/2022-05-02_14-34-35_q2x4aw.mp3",
+    label: "නිදිමතයි",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482461/2022-05-02_14-36-44_r5dtsf.mp3",
+    label: "තිබහයි",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482528/2022-05-02_14-38-12_o4oslm.mp3",
+    label: "බඩිගිනි",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482600/2022-05-02_14-39-21_vxrmj1.mp3",
+    label: "කොහොමද?",
+  },
+
+  // Slide 8 Audio
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651477331/2022-05-02_13-11-14_wqvdvs.mp3",
+    label: "ආයුබෝවන්",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651477331/2022-05-02_13-11-14_wqvdvs.mp3",
+    label: "ඔව්",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651481915/2022-05-02_14-27-37_bugyqg.mp3",
+    label: "නැහැ",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482089/2022-05-02_14-30-09_y5ilia.mp3",
+    label: "සුබ උදෑසනක්",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482230/2022-05-02_14-32-00_zdbvbg.mp3",
+    label: "සුබ රාත්‍රියක්",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482322/2022-05-02_14-34-35_q2x4aw.mp3",
+    label: "නිදිමතයි",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482461/2022-05-02_14-36-44_r5dtsf.mp3",
+    label: "තිබහයි",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482528/2022-05-02_14-38-12_o4oslm.mp3",
+    label: "බඩිගිනි",
+  },
+  {
+    sound: "https://res.cloudinary.com/avanoa/video/upload/v1651482600/2022-05-02_14-39-21_vxrmj1.mp3",
+    label: "කොහොමද?",
+  },  
 ];
+
 class phraseBubble extends Component {
   constructor(props) {
     super(props);
@@ -112,7 +384,7 @@ class phraseBubble extends Component {
   selectionFunc(phNum) {
     console.log(this.state.phrases[phNum]);
     this.soundPlay(audioClips[phNum].sound)
-  }
+  } 
 
   
   componentDidUpdate() {
@@ -122,44 +394,46 @@ class phraseBubble extends Component {
     if(index == 21){
       //this.selectBubble(0);
       //select1="selection"
-      this.selectBubble(0);
+      this.selectBubble(1);
       this.props.setCount(0);
     }
     if(index == 22){
-      this.selectBubble(1);
+      this.selectBubble(2);
     }
     if(index == 23){
-      this.selectBubble(2);
+      this.selectBubble(3);
 
     }
     if(index == 24){
-        this.selectBubble(3);
+        this.selectBubble(4);
     }
     if(index == 25){
       //select5 = "selection"
-      this.selectBubble(4);
+      this.selectBubble(5);
     }
     if(index == 26){
       //select6 = "selection"
-      this.selectBubble(5);
+      this.selectBubble(6);
     }
 
     if(index == 27){
       //select7 = "selection"
-      this.selectBubble(6);
+      this.selectBubble(7);
 
     }if(index == 28){
       //select8 = "selection"
-      this.selectBubble(7);
+      this.selectBubble(8);
 
     }if(index == 29){
       //select9 = "selection"
-      this.selectBubble(8);
+      this.selectBubble(9);
     }
 
     //31-40 slide 2
+
     
   }
+
 
   render() {
     const index = this.props.navList.index;
@@ -195,7 +469,7 @@ class phraseBubble extends Component {
     let ph8 = "සුබ උදෑසනක්"
     let ph9 = "සුබ උදෑසනක්"
 
-
+    
     if(index == 3 && counter == 0){
       //setSel("selectionBoarder");
       //this.setState({ slide1 : "selected"});
@@ -276,7 +550,7 @@ class phraseBubble extends Component {
 
     if(index == 13){
       slide1 = "selection"
-
+      
       if(counter == 0){
         select1 = "selected"
       }
@@ -312,99 +586,23 @@ class phraseBubble extends Component {
         select9 = "selected"
         select8 = "notSelected"
       }
-
     }
 
     
-    if(index == 14){
-      slide2 = "selection"
-      ph1 = "new slide 2"
-      ph2 = "new"
-      ph3 = "new"
-      ph4 = "new"
-      ph5 = "new"
-      ph6 = "new"
-      ph7 = "new"
-      ph8 = "new"
-      ph9 = "new"
-
-      if(counter == 0){
-        select1 = "selected"
-      }
-      if(counter == 1){
-        select2 = "selected"
-        select1 = "notSelected"
-      }
-      if(counter == 2){
-        select3 = "selected"
-        select2 = "notSelected"
-      }
-      if(counter == 3){
-        select4 = "selected"
-        select3 = "notSelected"
-      }
-      if(counter == 4){
-        select5 = "selected"
-        select4 = "notSelected"
-      }
-      if(counter == 5){
-        select6 = "selected"
-        select5 = "notSelected"
-      }
-      if(counter == 6){
-        select7 = "selected"
-        select6 = "notSelected"
-      }
-      if(counter == 7){
-        select8 = "selected"
-        select7 = "notSelected"
-      }
-      if(counter == 8){
-        select9 = "selected"
-        select8 = "notSelected"
-      }
-
-    }
-
-
-
-    if (index == 15){
-      slide3 = "selection"
-      ph1 = "new slide 3"
-      ph2 = "new"
-      ph3 = "new"
-      ph4 = "new"
-      ph5 = "new"
-      ph6 = "new"
-      ph7 = "new"
-      ph8 = "new"
-      ph9 = "new"
-     
-    }
-    if (index ==16){
-      slide4 = "selection"
-    }
-    if (index ==17){
-      slide5 = "selection"
-    
-    }
-    if (index ==18){
-      slide6 = "selection"
-    }
-    if (index ==19){
-      slide7 = "selection"
-    }
-    if (index ==20){
-      slide8 = "selection"
-    }
     
 
     return (
       <div>
         
         {/* {this.RenderButtonSound()} */}
-        <button className={slide1} >Slide 1</button><button className={slide2}>Slide 2</button><button className={slide3}>Slide 3</button><button className={slide4} >Slide 4</button>
-        <button className={slide5} >Slide 5</button><button className={slide6} >Slide 6</button><button className={slide7} >Slide 7</button><button className={slide8} >Slide 8</button>
+        <button className={slide1}>Slide 1</button>
+        <button className={slide2}>Slide 2</button>
+        <button className={slide3}>Slide 3</button>
+        <button className={slide4}>Slide 4</button>
+        <button className={slide5}>Slide 5</button>
+        <button className={slide6}>Slide 6</button>
+        <button className={slide7}>Slide 7</button>
+        <button className={slide8}>Slide 8</button>
         <div class="flex-container">
           <div className={select1}>{ph1}</div>
           <div className={select2}>{ph2}</div>
@@ -414,8 +612,7 @@ class phraseBubble extends Component {
           <div className={select6}>{ph6}</div>
           <div className={select7}>{ph7}</div>
           <div className={select8}>{ph8}</div>
-          <div className={select9}>{ph9}</div>
-          
+          <div className={select9}>{ph9}</div>          
         </div>
         {this.props.navList.index}
       </div>
