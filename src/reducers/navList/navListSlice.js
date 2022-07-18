85babafb864370844d14431c4e495f442717f2f1
nipunbandara <nipun.bandara@outlook.com>
2022-07-18 10:25:40 +0530

    navList updated for phrase bubbles

--- src/reducers/navList/navListSlice.js
+++ src/reducers/navList/navListSlice.js
@@ -3,8 +3,8 @@ import { createSlice } from '@reduxjs/toolkit'
 export const navListSlice = createSlice({
   name: 'navList',
   initialState: {
-    tree: [2, 5,5, 8,4,3,5,5, 0,0,0,2,3, 10,10,9,9,9,9,9,8],
-    treeLayers : [1, 3,8, 13,null,null,null,null, null,null,null,121,123, 21,31,41,50,60,70,80,89],
+    tree: [2, 5,5, 8,4,3,5,5, 0,0,0,2,3, 9,9,9,9,9,9,9,9],
+    treeLayers : [1, 3,8, 13,null,null,null,null, null,null,null,121,123, 21,31,41,51,61,71,81,91],
     indexCount  : {9: 1, 10 : 2, 11 : 3, 12 : 4, 14 : 1, 15 : 2, 16 : 3, 17:4, 18:5, 19:6, 20:7},
     index : 0,
     preIndex : 0,
