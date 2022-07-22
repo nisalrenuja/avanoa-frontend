7294de8b6db0bfea742dc5ab3af546fd080d9505
nisalrenuja <nrenuja@gmail.com>
2022-07-22 23:30:57 +0530

    structured

--- src/Layouts/EmergencyLayout/EmergencyLayout.js
+++ src/Layouts/EmergencyLayout/EmergencyLayout.js
@@ -1,90 +1,108 @@
 import React from "react";
-import emailjs from '@emailjs/browser';
+// import emailjs from '@emailjs/browser';
 
 class EmergencyLayout extends React.Component {
 	constructor(props) {
 		super(props);
-		
+
 		this.state = {
-			recepient: 'Recepient',
+			recepient: "Recepient",
 			email: "test@gmail.com",
-			subject: 'Subject',
-			message: 'Message',
+			subject: "Subject",
+			message: "Message",
 			Brecepient: false,
 			Bmessage: false,
 			BFinalMessage: false,
-			page: 1
+			page: 1,
 		};
 
 		this.handleClick = this.handleClick.bind(this);
-		this.handleBack = this.handleBack.bind(this);    
-		this.sendEmail = this.sendEmail.bind(this);     
+		this.handleBack = this.handleBack.bind(this);
+		this.sendEmail = this.sendEmail.bind(this);
 	}
 
 	handleBack(event) {
 		event.preventDefault();
-		if (event.target.name == "messageBack"){      
-			this.setState({page: 1});    
+		if (event.target.name == "messageBack") {
+			this.setState({ page: 1 });
 		}
-		if (event.target.name == "finalMessageBack"){        
-			this.setState({page: 2});    
-		}    
+		if (event.target.name == "finalMessageBack") {
+			this.setState({ page: 2 });
+		}
 	}
 
 	handleClick(event) {
 		event.preventDefault();
-			
-		this.setState({[event.target.name]: event.target.value});
 
-		if (event.target.name == "recepient"){
+		this.setState({ [event.target.name]: event.target.value });
 
-			this.setState({Brecepient: true});
+		if (event.target.name == "recepient") {
+			this.setState({ Brecepient: true });
 
-			if (event.target.value == "Care Taker"){
-				this.setState({email: "nipunchamodya@gmail.com"});
+			if (event.target.value == "Care Taker") {
+				this.setState({ email: "nipunchamodya@gmail.com" });
+			} else if (event.target.value == "Gurdian") {
+				this.setState({ email: "pokirisa@gmail.com" });
+			} else if (event.target.value == "Doctor") {
+				this.setState({ email: "pokirisa@gmail.com" });
 			}
 
-			else if (event.target.value == "Gurdian"){
-				this.setState({email: "pokirisa@gmail.com"});
-			}
-
-			else if (event.target.value == "Doctor"){
-				this.setState({email: "pokirisa@gmail.com"});
-			}
-
-			this.setState({page: 2});      
+			this.setState({ page: 2 });
 		}
 
-		if (event.target.name == "message"){
-			this.setState({Bmessage: true});
-			this.setState({page: 3});
-			this.setState({subject: event.target.value});
-		}     
+		if (event.target.name == "message") {
+			this.setState({ Bmessage: true });
+			this.setState({ page: 3 });
+			this.setState({ subject: event.target.value });
+		}
 	}
 
-	sendEmail = (e) => {
-		e.preventDefault();
+	// sendEmail = (e) => {
+	// 	e.preventDefault();
 
-		emailjs.send('service_s63wkq9', 'template_t7hy137', this.state, 'zXAlVVivBD7BlgIVm')
-		.then((result) => {
-			console.log(result.text);
-		}, (error) => {
-			console.log(error.text);
-		});
-	};
+	// 	emailjs
+	// 		.send(
+	// 			"service_s63wkq9",
+	// 			"template_t7hy137",
+	// 			this.state,
+	// 			"zXAlVVivBD7BlgIVm"
+	// 		)
+	// 		.then(
+	// 			(result) => {
+	// 				console.log(result.text);
+	// 			},
+	// 			(error) => {
+	// 				console.log(error.text);
+	// 			}
+	// 		);
+	// };
 
-	render() {								
-
+	render() {
 		let comp;
 
-		if(this.state.page === 1){
-			comp = <Recepient handleClick={this.handleClick}/>;
+		if (this.state.page === 1) {
+			comp = <Recepient handleClick={this.handleClick} />;
 		}
 		if (this.state.page === 2) {
-			comp = <Message recepient={this.state.recepient} handleClick={this.handleClick} handleBack={this.handleBack} />;
+			comp = (
+				<Message
+					recepient={this.state.recepient}
+					handleClick={this.handleClick}
+					handleBack={this.handleBack}
+				/>
+			);
 		}
-		if (this.state.page === 3){
-			comp = <FinalMessage  recepient={this.state.recepient} email={this.state.email} subject={this.state.subject} message={this.state.message} handleBack={this.handleBack} sendEmail={this.sendEmail} />;
+		if (this.state.page === 3) {
+			comp = (
+				<FinalMessage
+					recepient={this.state.recepient}
+					email={this.state.email}
+					subject={this.state.subject}
+					message={this.state.message}
+					handleBack={this.handleBack}
+					sendEmail={this.sendEmail}
+				/>
+			);
 		}
 
 		function Recepient(props) {
@@ -92,52 +110,108 @@ class EmergencyLayout extends React.Component {
 				<div>
 					<h1>Select Recepient</h1>
 
-					<button onClick={props.handleClick} name="recepient" value="Care Taker">Care Taker</button>
-					<button onClick={props.handleClick} name="recepient" value="Gurdian">Gurdian</button>
-					<button onClick={props.handleClick} name="recepient" value="Doctor">Doctor</button>
+					<button
+						onClick={props.handleClick}
+						name="recepient"
+						value="Care Taker"
+					>
+						Care Taker
+					</button>
+					<button onClick={props.handleClick} name="recepient" value="Gurdian">
+						Gurdian
+					</button>
+					<button onClick={props.handleClick} name="recepient" value="Doctor">
+						Doctor
+					</button>
 				</div>
 			);
-		}    
+		}
 
-		function Message (props) {
-			console.log("Message");      
-			if (props.recepient == "Care Taker"){
+		function Message(props) {
+			console.log("Message");
+			if (props.recepient == "Care Taker") {
 				return (
 					<div>
 						<h1>Select Message</h1>
 
-						<button onClick={props.handleClick} name="message" value="I need Medicine">I need Medicine</button>
-						<button onClick={props.handleClick} name="message" value="I need to Eat">I need to Eat</button>
-						<button onClick={props.handleClick} name="message" value="I am Thirsty">I am Thirsty</button>
+						<button
+							onClick={props.handleClick}
+							name="message"
+							value="I need Medicine"
+						>
+							I need Medicine
+						</button>
+						<button
+							onClick={props.handleClick}
+							name="message"
+							value="I need to Eat"
+						>
+							I need to Eat
+						</button>
+						<button
+							onClick={props.handleClick}
+							name="message"
+							value="I am Thirsty"
+						>
+							I am Thirsty
+						</button>
 
-						<button onClick={props.handleBack} name="messageBack">Back</button>
+						<button onClick={props.handleBack} name="messageBack">
+							Back
+						</button>
+					</div>
+				);
+			} else if (props.recepient == "Gurdian") {
+				return (
+					<div>
+						<h1>Select Message</h1>
+
+						<button
+							onClick={props.handleClick}
+							name="message"
+							value="I need to See you"
+						>
+							I need to See you
+						</button>
+						<button
+							onClick={props.handleClick}
+							name="message"
+							value="The Care Taker is not here"
+						>
+							The Care Taker is not here
+						</button>
+
+						<button onClick={props.handleBack} name="messageBack">
+							Back
+						</button>
+					</div>
+				);
+			} else if (props.recepient == "Doctor") {
+				return (
+					<div>
+						<h1>Select Message</h1>
+
+						<button
+							onClick={props.handleClick}
+							name="message"
+							value="I am not feeling well"
+						>
+							I am not feeling well
+						</button>
+						<button
+							onClick={props.handleClick}
+							name="message"
+							value="My appointment is coming up"
+						>
+							My appointment is coming up
+						</button>
+
+						<button onClick={props.handleBack} name="messageBack">
+							Back
+						</button>
 					</div>
 				);
 			}
-			else if (props.recepient == "Gurdian"){
-				return (
-					<div>
-						<h1>Select Message</h1>
-
-						<button onClick={props.handleClick} name="message" value="I need to See you">I need to See you</button>
-						<button onClick={props.handleClick} name="message" value="The Care Taker is not here">The Care Taker is not here</button>
-						
-						<button onClick={props.handleBack} name="messageBack">Back</button>
-					</div>
-				);
-			}
-			else if (props.recepient == "Doctor"){
-				return (
-					<div>
-						<h1>Select Message</h1>
-
-						<button onClick={props.handleClick} name="message" value="I am not feeling well">I am not feeling well</button>
-						<button onClick={props.handleClick} name="message" value="My appointment is coming up">My appointment is coming up</button>
-						
-						<button onClick={props.handleBack} name="messageBack">Back</button>
-					</div>
-				);
-			} 
 		}
 
 		function FinalMessage(props) {
@@ -145,24 +219,23 @@ class EmergencyLayout extends React.Component {
 				<div>
 					<h1>Final Message</h1>
 
-					<p >Recepient - {props.recepient}</p>
-					<p >Email - {props.email}</p>
-					<p >Subject - {props.subject}</p>
-					<p >Message - {props.message}</p>
+					<p>Recepient - {props.recepient}</p>
+					<p>Email - {props.email}</p>
+					<p>Subject - {props.subject}</p>
+					<p>Message - {props.message}</p>
 
-					<button onClick={props.handleBack} name="finalMessageBack">Back</button>
-					<button name="finalMessageSend" onClick={props.sendEmail}>Send</button>
+					<button onClick={props.handleBack} name="finalMessageBack">
+						Back
+					</button>
+					<button name="finalMessageSend" onClick={props.sendEmail}>
+						Send
+					</button>
 				</div>
 			);
 		}
-		
-		return (
-			<div>
-				{comp}				
-			</div>
-		);
-	};
+
+		return <div>{comp}</div>;
+	}
 }
 
 export default EmergencyLayout;
-
