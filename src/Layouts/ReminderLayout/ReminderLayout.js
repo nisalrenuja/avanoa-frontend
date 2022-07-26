50f369f8e3c1b618a2ad2153a8763c8c13341718
maneeshakaravita99 <maneeshakaravita@gmail.com>
2022-07-26 12:30:07 +0530

    adding new reminders updated

--- src/Layouts/ReminderLayout/ReminderLayout.js
+++ src/Layouts/ReminderLayout/ReminderLayout.js
@@ -4,62 +4,52 @@ import FormInput from "../../components/Reminder/FormInput";
 
 const ReminderLayout = () => {
   const [values, setValues] = useState({
-    username: "",
-    email: "",
-    birthday: "",
-    password: "",
-    confirmPassword: "",
+    title: "",
+    details: "",
+    date: "",
+    time: "",
+    
   });
 
   const inputs = [
     {
       id: 1,
-      name: "username",
+      name: "title",
       type: "text",
-      placeholder: "Username",
+      placeholder: "title",
       errorMessage:
         "Username should be 3-16 characters and shouldn't include any special character!",
-      label: "Username",
+      label: "title",
       pattern: "^[A-Za-z0-9]{3,16}$",
       required: true,
     },
     {
       id: 2,
-      name: "email",
-      type: "email",
-      placeholder: "Email",
-      errorMessage: "It should be a valid email address!",
-      label: "Email",
+      name: "details",
+      type: "details",
+      placeholder: "details",
+      errorMessage: "It should be a valid format!",
+      label: "details",
       required: true,
     },
     {
       id: 3,
-      name: "birthday",
+      name: "date",
       type: "date",
-      placeholder: "Birthday",
-      label: "Birthday",
+      placeholder: "date",
+      label: "date",
     },
     {
       id: 4,
-      name: "password",
-      type: "password",
-      placeholder: "Password",
+      name: "time",
+      type: "time",
+      placeholder: "time",
       errorMessage:
-        "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
-      label: "Password",
+        "time should be in valid format!",
+      label: "time",
       pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
       required: true,
-    },
-    {
-      id: 5,
-      name: "confirmPassword",
-      type: "password",
-      placeholder: "Confirm Password",
-      errorMessage: "Passwords don't match!",
-      label: "Confirm Password",
-      pattern: values.password,
-      required: true,
-    },
+    }
   ];
 
   const handleSubmit = (e) => {
@@ -73,7 +63,7 @@ const ReminderLayout = () => {
   return (
     <div className="app">
       <form onSubmit={handleSubmit}>
-        <h1>Register</h1>
+        <h1>New Reminder</h1>
         {inputs.map((input) => (
           <FormInput
             key={input.id}
