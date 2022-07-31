825f26f717ae302faa92a1a74cb2c9b5df219429
nipunbandara <nipun.bandara@outlook.com>
2022-08-01 00:30:23 +0530

    Initial update for EE navigation

--- src/reducers/navList/navListSlice.js
+++ src/reducers/navList/navListSlice.js
@@ -4,7 +4,9 @@ export const navListSlice = createSlice({
   name: 'navList',
   initialState: {
     tree: [2, 5,5, 8,3,3,5,5, 0,0,0,2,3, 9,9,9,9,9,9,9,9, 4, 2],
+    treeExc : {150 : 4, 153 : 2},
     treeLayers : [1, 3,8, 13,200,150,null,null, null,null,null,121,123, 21,31,41,51,61,71,81,91, 153, 170],
+    treeLayersExc : {150 : 153, 153 : 170},
     indexCount : {9: 1, 10 : 2, 11 : 3, 12 : 4, 14 : 1, 15 : 2, 16 : 3, 17:4, 18:5, 19:6, 20:7},
     index : 0,
     preIndex : 0,
