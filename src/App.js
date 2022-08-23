57f0d1223873c06c6b17f13e17584e6d6b5a4793
Randimal Fernando <randimalfdo@gmail.com>
2022-08-23 14:12:30 +0530

    Responsive files

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import Edit from "./components/Reminder/Edit";
 import Details from "./components/Reminder/Details";
 import { Settings } from "@mui/icons-material";
 import SettingsLayout from "./Layouts/SettingsLayout/SettingsLayout";
+import { useMediaQuery } from 'react-responsive';
 
 function App() {
 	const { dispatch, token, isLoggedIn } = useContext(AuthContext);
@@ -44,6 +45,26 @@ function App() {
 			getUser();
 		}
 	}, [dispatch, token]);
+
+	const isMobileDevice = useMediaQuery({
+		query: "(min-device-width: 480px)",
+	  });
+	
+	  const isTabletDevice = useMediaQuery({
+		query: "(min-device-width: 768px)",
+	  });
+	
+	  const isLaptop = useMediaQuery({
+		query: "(min-device-width: 1024px)",
+	  });
+	
+	  const isDesktop = useMediaQuery({
+		query: "(min-device-width: 1200px)",
+	  });
+	
+	  const isBigScreen = useMediaQuery({
+		query: "(min-device-width: 1201px )",
+	  });
 
 	return (
 		<BrowserRouter>
---
+++ src/components/mobile/tablet-mobile.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./tablet-mobile.styles.css";
+export const TabletMobile = () => {
+  return (
+    <div className="tablet-mobile">
+      <p>Whoops! I'm in tablet-mobile mode.</p>
+      <p>But if you see anything below me, i am now in Desktop mode</p>
+    </div>
+  );
+};
