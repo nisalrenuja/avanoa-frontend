ca1030fe78d88e9fd97c7ef9569e62268020d99f
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2022-10-04 19:15:44 +0530

    radio button added

--- src/components/AudioSetting/AudioSetting.js
+++ src/components/AudioSetting/AudioSetting.js
@@ -4,16 +4,33 @@ import "./AudioSetting.css";
 const AudioSetting = () => {
   return (
     <div>
-      Hello
       <br></br>
       <h1 style={{ color: "white" }}>Audio Settings</h1>
+
+      <h2 style={{ color: "white" }}>Gender Select</h2>
+      <div style={{ color: "white" }}>
+        <input
+          type="radio"
+          value="Male"
+          name="gender"
+          style={{ color: "white" }}
+        />{" "}
+        Male<br></br>
+        <input
+          type="radio"
+          value="Female"
+          name="gender"
+          style={{ color: "white" }}
+        />{" "}
+        Female<br></br>
+      </div>
       <br></br>
       <br></br>
-      <h2 style={{ color: "white" }}>Speaker</h2>
+      <h2 style={{ color: "white" }}>Azure Volume</h2>
       <input type="range" min="1" max="100" class="slider" id="myRange"></input>
       <br></br>
       <br></br>
-      <h2 style={{ color: "white" }}>Microphone</h2>
+      <h2 style={{ color: "white" }}>Reset</h2>
       <input type="range" min="1" max="100" class="slider" id="myRange"></input>
     </div>
   );
