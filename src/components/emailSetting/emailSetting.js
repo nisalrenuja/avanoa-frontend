e025d613c5d5df948d0649597715ce5332021cba
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2022-10-11 19:14:22 +0530

    comeented in eme

--- src/components/emailSetting/emailSetting.js
+++ src/components/emailSetting/emailSetting.js
@@ -1,13 +1,29 @@
-import React from 'react'
+// import React from 'react'
+// import { useState } from "react";
 
-const EmailSetting = () => {
-  return (
-    <div>
-      <h1>Email Settings</h1>
-      <h1>Enter User Email</h1>
-      <input type="text" placeholder="Enter Email"></input>
-    </div>
-  )
-}
+// const EmailSetting = () => {
 
-export default EmailSetting
+//   const handleChange = (e) => {
+//     console.log(e.target.value);
+//     if (e.target.value === "Male") {
+//       localStorage.setItem("vgender", "Male");
+//       console.log(localStorage.getItem("vgender"));
+//     } else if (e.target.value === "Female") {
+//       localStorage.setItem("gender", "Female");
+//       console.log(localStorage.getItem("vgender"));
+//     } else {
+//       console.log("Not Defined");
+//       console.log(localStorage.getItem("vgender"));
+//     }
+//   };
+//   return (
+//     <div>
+//       <h1>Email Settings</h1>
+//       <h1>Enter User Email</h1>
+//       <input type="text" placeholder="Enter Email" onChange={handleChange}></input>
+//       <button onChange={handleChange}>Submit</button>
+//     </div>
+//   )
+// }
+
+// export default EmailSetting
